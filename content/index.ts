3387ea8f65fb78fa795825f77e3f7c27c99e6c31
jeff2857 <jeffliu2857@gmail.com>
2024-08-03 00:07:34 +0800

    feat: render Solana blinks in X

--- content/index.ts
+++ content/index.ts
@@ -32,132 +32,143 @@ interface ActionError {
 }
 
 const PROTOCOL_PREFIX = 'btc-action:';
+const SOL_ACTION = 'solana-action:';
 
-const isDiscord = document.URL.includes('https://discord.com')
+const isDiscord = document.URL.includes('https://discord.com');
 
 async function sha256(message: string) {
     // encode as UTF-8
-    const msgBuffer = new TextEncoder().encode(message)
+    const msgBuffer = new TextEncoder().encode(message);
 
     // hash the message
-    const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer)
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
 
     // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
 
     // convert bytes to hex string
-    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
-    return hashHex
+    const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('');
+    return hashHex;
 }
 
 const renderActions = async (actionUrl: string, element: Element) => {
-    const res = await fetch(actionUrl)
+    const res = await fetch(actionUrl);
     const resJson: ActionGetResponse = await res.json();
 
-    const iconElem = document.createElement('img')
-    iconElem.className = 'btc-action_icon'
-    iconElem.src = resJson.icon
-    iconElem.alt = 'action-image'
+    const iconElem = document.createElement('img');
+    iconElem.className = 'btc-action_icon';
+    iconElem.src = resJson.icon;
+    iconElem.alt = 'action-image';
 
-    const titleElem = document.createElement('div')
-    titleElem.innerHTML = resJson.title
-    titleElem.className = 'btc-action_title'
+    const titleElem = document.createElement('div');
+    titleElem.innerHTML = resJson.title;
+    titleElem.className = 'btc-action_title';
 
-    const descElem = document.createElement('div')
-    descElem.innerHTML = resJson.description
+    const descElem = document.createElement('div');
+    descElem.innerHTML = resJson.description;
 
-    const linksElem = document.createElement('div')
-    linksElem.className = 'btc-action_links'
+    const linksElem = document.createElement('div');
+    linksElem.className = 'btc-action_links';
     if (resJson.links) {
-        const { actions } = resJson.links
+        const { actions } = resJson.links;
         for (let action of actions) {
-            const btnElem = document.createElement('div')
-            btnElem.className = 'btc-action_btn'
-            btnElem.innerHTML = action.label
-            btnElem.style.width = `${Math.floor(360 / resJson.links.actions.length)}px`
-            linksElem.appendChild(btnElem)
+            const btnElem = document.createElement('div');
+            btnElem.className = 'btc-action_btn';
+            btnElem.innerHTML = action.label;
+            btnElem.style.width = `${Math.floor(360 / resJson.links.actions.length)}px`;
+            linksElem.appendChild(btnElem);
             btnElem.onclick = async (e) => {
-                e.preventDefault()
-                e.stopPropagation()
+                e.preventDefault();
+                e.stopPropagation();
                 const res = await fetch(actionUrl, {
                     method: 'POST',
                     headers: {
-                        'Content-Type': 'application/json'
+                        'Content-Type': 'application/json',
                     },
-                    mode: 'no-cors'
+                    mode: 'no-cors',
                 });
-                console.log({ res })
+                console.log({ res });
                 const resJson = await res.json();
-                console.log({ resJson })
-                alert(resJson.toString())
+                console.log({ resJson });
+                alert(resJson.toString());
                 // todo: call unisat to send tx
-            }
+            };
         }
     }
 
-    const actionElem = document.createElement('div')
-    actionElem.className = 'btc-action_card'
-    actionElem.appendChild(iconElem)
-    actionElem.appendChild(titleElem)
-    actionElem.appendChild(descElem)
-    actionElem.appendChild(linksElem)
+    const actionElem = document.createElement('div');
+    actionElem.className = 'btc-action_card';
+    actionElem.appendChild(iconElem);
+    actionElem.appendChild(titleElem);
+    actionElem.appendChild(descElem);
+    actionElem.appendChild(linksElem);
 
-    element.parentNode?.appendChild(actionElem)
-    element.remove()
-}
+    element.parentNode?.appendChild(actionElem);
+    element.remove();
+};
 
-const isValidElement = (tag: string, element: Element) => {
-    if (tag === "code" && element.innerHTML.includes(PROTOCOL_PREFIX)) return true
-    if (tag === "span" && element.textContent?.includes(PROTOCOL_PREFIX)) return true
-    return false
-}
+const isValidElement = (elem: Element): Element | null => {
+    const aLink = elem.querySelector('a');
+    return aLink;
+};
 
-const handleElement = async (tag: string, element: Element) => {
+const extractMetadata = (res: string) => {
+    const startIdx = res.indexOf(SOL_ACTION);
+    if (startIdx < 0) {
+        return;
+    }
+    const endIdx = res.slice(startIdx).indexOf('"');
+    if (endIdx < 0) return;
+    return res.slice(startIdx, startIdx + endIdx);
+};
+
+const getActionUrl = async (url: string) => {
     try {
-        const bal = tag === "code" ? element.innerHTML : element.textContent;
-        if (!bal) return
+        const res = await fetch(url);
+        const resJson = await res.text();
+        const actionUrl = extractMetadata(resJson);
+        return actionUrl;
+    } catch (err: any) {
+        console.error(err);
+    }
+};
 
-        const url = bal?.split(PROTOCOL_PREFIX)[1]
-        if (!url || url.length === 0) return
+const handleElement = async (elem: Element, parent: Element) => {
+    const link = elem.getAttribute('href');
+    if (!link) return;
+    const actionUrl = await getActionUrl(link);
+    if (!actionUrl) return;
+    try {
+        const url = actionUrl.split(SOL_ACTION)[1];
+        if (!url || url.length === 0) return;
 
-        const hash = await sha256(url)
-        const newId = `bal-cs-${hash}`
+        const hash = await sha256(url);
+        const newId = `bal-cs-${hash}`;
 
         // already unfurled
-        if (element.parentNode?.querySelector(`#${newId}`)) return
+        if (elem.parentNode?.querySelector(`#${newId}`)) return;
 
-        console.log(`unfurl: ${url}`)
-        /*
-        const newIframe = document.createElement("iframe")
-        const encodedEAL = encodeURIComponent(url.replaceAll("&amp;", "&"))
+        console.log(`unfurl: ${url}`);
 
-        newIframe.src = `chrome-extension://dkfjmmpblkmjonmaemcmngophheaolkh/tabs/eal.html?action=${encodedEAL}`
-        newIframe.width = "430"
-        newIframe.height = "430"
-        // newIframe.style.backgroundColor = "white"
-        newIframe.style.marginTop = "8px"
-        newIframe.style.border = "none"
-
-        element.parentNode?.appendChild(newIframe)
-        element.remove()
-        */
-
-        renderActions(url, element)
+        (parent as HTMLElement).style.border = 'none';
+        renderActions(url, elem);
         // element.remove()
     } catch (error) {
-        console.error(error, "[Shortcut] Error")
+        console.error(error, '[Shortcut] Error');
     }
-}
+};
 
-const searchElements = async (tag: string) => {
-    const tags = document.querySelectorAll(tag)
-
-    for (const element of tags) {
-        if (isValidElement(tag, element)) {
-            await handleElement(tag, element)
+const searchLinkCard = async () => {
+    const elems = document.querySelectorAll('div[data-testid="card.wrapper"]');
+    for (const elem of elems) {
+        const aTag = isValidElement(elem);
+        if (aTag) {
+            await handleElement(aTag, elem);
         }
     }
-}
+};
 
 /*
 chrome.tabs.onUpdated.addListener((tabId: any, changeInfo: any) => {
@@ -171,16 +182,33 @@ chrome.tabs.onUpdated.addListener((tabId: any, changeInfo: any) => {
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         setTimeout(() => {
-            console.log('load')
-            searchElements(isDiscord ? 'code' : 'span')
-        }, 3000)
+            console.log('load');
+            // searchElements(isDiscord ? 'code' : 'span');
+            searchLinkCard();
+        }, 5000);
     }
-}
+};
 
 //searchElements("code")
+/*
 if (isDiscord) {
     document.addEventListener("mouseover", () => searchElements("code"))
 } else {
     document.addEventListener("scroll", () => searchElements("span"))
 }
+*/
 
+/*
+const fetchTest = async () => {
+    console.log('fetch------------')
+    try {
+        const res = await fetch('https://t.co/VepaaM2mBD');
+        const resJson = await res.text()
+        console.log({ resJson })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+fetchTest()
+*/
